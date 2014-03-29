c287aee73201c58136911381f09fb7be8f03a2cb
dhacker29 <dhackerdvm@gmail.com>
2014-03-29 04:36:22 -0400

    Update audio config
    Change-Id: I7b5858db0a410053d9fba88a54608c99ed31cb58

--- msm8226.mk
+++ msm8226.mk
@@ -25,7 +25,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_PACKAGES += \
     audio_policy.msm8226 \
     libaudioparameter \
-    libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
 
